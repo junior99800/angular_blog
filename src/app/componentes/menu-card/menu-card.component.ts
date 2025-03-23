00ba735b75d010a8e1f77533597b8e2cf0fdc4da
junior99800 <juniorcarros36@hotmail.com>
2025-03-23 17:50:34 -0300

    content created

--- src/app/componentes/menu-card/menu-card.component.ts
+++ src/app/componentes/menu-card/menu-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-card',
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './menu-card.component.html',
   styleUrl: './menu-card.component.css'
 })
-export class MenuCardComponent {
+export class MenuCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string= ""
+  @Input()
+  cardDescription:string =""
+  @Input()
+  Id:string="0"
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
